48ad4693c05459ce7544a76fb487b49106a69f7c
Riley Lathan <rileylatha202@gmail.com>
2021-10-12 18:07:40 -0400

    very basics of node

---
+++ app.js
@@ -0,0 +1,7 @@
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) { 
+        console.log(profileDataArr[i]);
+    }
+};
+
+printProfileData(profileDataArgs)
