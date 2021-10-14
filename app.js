a4adea0f231fedf05d1c1f25fd433d04670104f4
Riley Lathan <rileylatha202@gmail.com>
2021-10-14 18:13:40 -0400

    generated an html file

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) { 
-        console.log(profileDataArr[i]);
-    }
-};
+const fs = require('fs');
 
-printProfileData(profileDataArgs)
+const generatePage = require('./src/page-template.js')
+
+const profileDataArgs = process.argv.slice(2);
+
+
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
